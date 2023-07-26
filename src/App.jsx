be6947c5e27240628b60c5605b4d0634c91513cd
Maxgg21 <lionel.gegena@gmail.com>
2023-07-25 21:30:39 -0300

    Colocando drinksProvider en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { CategoriesProvider } from "./context/CategoriesProvider";
+import { DrinksProvider } from "./context/DrinksProvider";
 import MainLayout from "./loyaut";
 import AppRoutes from "./routes";
 
@@ -6,9 +7,11 @@ import AppRoutes from "./routes";
 function App() {
   return (
     <MainLayout>
+      <DrinksProvider>
       <CategoriesProvider>
         <AppRoutes/>
       </CategoriesProvider>
+    </DrinksProvider>
     </MainLayout>
   )
 }
