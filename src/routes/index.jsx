09123d0a7a34288912f68eeea85b2be3db935e08
Maxgg21 <lionel.gegena@gmail.com>
2023-08-19 18:52:41 -0300

    Agregando pages de longIn y longUp usando material Ui, se componetiso el copyrigth

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../page/Home";
 import NotFound from "../page/NotFound";
+import SignUp from "../page/User/SingUp";
+import SignIn from "../page/User/SingIn";
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
